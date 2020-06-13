19bdcbf64b54a976e6b776b98bb91d2301d6817d
john-rock <johnmrock.jr@gmail.com>
2020-06-13 13:22:43 -0400

    Create env variables

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 // Import routes
 const authRoutes = require('./routes/auth');
+
+// App middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
 // Middleware
 app.use('/api', authRoutes);
