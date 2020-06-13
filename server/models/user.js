2de0166f6616ebdc74a6e77429f2bb48845c76e8
john-rock <johnmrock.jr@gmail.com>
2020-06-13 13:05:44 -0400

    Build user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema(
     {
@@ -36,5 +37,43 @@ const userSchema = new mongoose.Schema(
 );
 
 // Virtual fields
+// Get password from front end
+userSchema
+    .virtual('password')
+    .set(function (password) {
+        // Create temp variable _password
+        this._password = password;
+        // Generate sale
+        this.salt = this.makeSalt();
+        // Encrypt password
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
 // Methods > authenticate, encryptPassword, makeSalt
+userSchema.methods = {
+    authenticate: function (plaintText) {
+        return this.encryptPassword(plaintText) === this.hashed_password;
+    },
+
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+
+    makeSalt: function () {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    },
+};
+
 // Export user model
+module.exports = mongoose.model('User', userSchema);
