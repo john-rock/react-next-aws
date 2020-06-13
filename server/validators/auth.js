eebb80aab76471b1051295d3b950bc478e3788d5
john-rock <johnmrock.jr@gmail.com>
2020-06-13 14:53:33 -0400

    Start building server validation wip

---
+++ server/validators/auth.js
@@ -0,0 +1,15 @@
+const { check } = require('express-validator');
+
+// Checks
+exports.userRegisterValidation = [
+    check('username')
+        .not()
+        .isEmpty()
+        .withMessage('Username is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+    check('password')
+        .isLength({min: 6})
+        .withMessage('Password must be at least 6 characters long')
+];
